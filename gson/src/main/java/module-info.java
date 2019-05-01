e5cf93e76de1e89b9d53935a95846ee702c9c651
Wael <32783348+dimwael@users.noreply.github.com>
2019-05-01 15:36:45 -0400

    Update module-info.java

--- gson/src/main/java/module-info.java
+++ gson/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.google.gson {
 	exports com.google.gson;
 	exports com.google.gson.annotations;
 	exports com.google.gson.reflect;
+	
 	exports com.google.gson.stream;
-
 	requires transitive java.sql;
 }
