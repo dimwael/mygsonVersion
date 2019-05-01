00caeddd3890f5fb032e7bfb1b125640101847f2
Wael <32783348+dimwael@users.noreply.github.com>
2019-05-01 15:27:31 -0400

    Update SerializationBenchmark.java

--- metrics/src/main/java/com/google/gson/metrics/SerializationBenchmark.java
+++ metrics/src/main/java/com/google/gson/metrics/SerializationBenchmark.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2011 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.google.gson.metrics;
 
 import com.google.caliper.Param;
@@ -21,13 +7,6 @@ import com.google.caliper.SimpleBenchmark;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-/**
- * Caliper based micro benchmarks for Gson serialization
- *
- * @author Inderjeet Singh
- * @author Jesse Wilson
- * @author Joel Leitch
- */
 public class SerializationBenchmark extends SimpleBenchmark {
 
   private Gson gson;
@@ -48,6 +27,13 @@ public class SerializationBenchmark extends SimpleBenchmark {
   public void timeObjectSerialization(int reps) {
     for (int i=0; i<reps; ++i) {
       gson.toJson(bag);
+    }
+  }
+  
+   public void timeObjectSerializations(int reps, int number) {
+    for (int i=0; i<reps; ++i) {
+      gson.toJson(bag);
+      number +=2;
     }
   }
 }
