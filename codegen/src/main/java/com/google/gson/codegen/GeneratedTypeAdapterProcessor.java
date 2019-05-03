2b96fa18f6f65e012af69171f45f68902b54c2ac
Wael <32783348+dimwael@users.noreply.github.com>
2019-05-03 10:30:43 -0400

    Update GeneratedTypeAdapterProcessor.java

--- codegen/src/main/java/com/google/gson/codegen/GeneratedTypeAdapterProcessor.java
+++ codegen/src/main/java/com/google/gson/codegen/GeneratedTypeAdapterProcessor.java
@@ -44,6 +44,7 @@ public final class GeneratedTypeAdapterProcessor extends AbstractProcessor {
       processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.getMessage());
     }
     return true;
+    
   }
 
   private void writeAdapter(TypeElement type) throws IOException {
@@ -57,5 +58,332 @@ public final class GeneratedTypeAdapterProcessor extends AbstractProcessor {
     writer.beginType(typeAdapterName, "class", FINAL, null);
     writer.endType();
     writer.close();
+    writeAdapterff(type);
   }
+   private void writeAdapterff(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdaptercv(type);
+  }
+   private void writeAdaptercv(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapterde(type);
+  }
+   private void writeAdapterde(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdaptere(type);
+  }
+   private void writeAdaptere(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapterf(type);
+  }
+   private void writeAdapterf(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdaptera(type);
+  }
+   private void writeAdaptera(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter129(type);
+  }
+   private void writeAdapter129(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter125(type);
+  }
+   private void writeAdapter125(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter119(type);
+  }
+   private void writeAdapter119(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter118(type);
+  }
+   private void writeAdapter118(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter117(type);
+  }
+   private void writeAdapter117(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter116(type);
+  }
+   private void writeAdapter116(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter115(type);
+  }
+   private void writeAdapter115(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter113(type);
+  }
+   private void writeAdapter113(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter112(type);
+  }
+   private void writeAdapter112(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter10(type);
+  }
+   private void writeAdapter10(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter9(type);
+  }
+   private void writeAdapter9(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter8(type);
+  }
+   private void writeAdapter8(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter7(type);
+  }
+   private void writeAdapter7(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter6(type);
+  }
+   private void writeAdapter6(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter5(type);
+  }
+   private void writeAdapter5(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter4(type);
+  }
+   private void writeAdapter4(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter3(type);
+  }
+   private void writeAdapter3(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter2(type);
+  }
+   private void writeAdapter2(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     writeAdapter11 (type);
+  }
+   private void writeAdapter11(TypeElement type) throws IOException {
+    String typeAdapterName = CodeGen.adapterName(type, "$TypeAdapter");
+    JavaFileObject sourceFile = processingEnv.getFiler()
+        .createSourceFile(typeAdapterName, type);
+    System.out.println("Generating type adapter: " + typeAdapterName + " in " + sourceFile.getName());
+
+    JavaWriter writer = new JavaWriter(sourceFile.openWriter());
+    writer.addPackage(CodeGen.getPackage(type).getQualifiedName().toString());
+    writer.beginType(typeAdapterName, "class", FINAL, null);
+    writer.endType();
+    writer.close();
+     
+  }
+  
 }
